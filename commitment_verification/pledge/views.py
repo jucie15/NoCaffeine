d2fd2618265973a327e2c8e0c8841452708d5957
gustos <jucie15@nate.com>
2017-05-11 19:59:36 +0900

    좋아요/싫어요 구현

--- commitment_verification/pledge/views.py
+++ commitment_verification/pledge/views.py
@@ -1,11 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from pledge.models import *
 from pledge.forms import CommentForm
-
+import json
 
 def index(request):
     # 인덱스
@@ -31,9 +33,9 @@ def congressman_list(request):
 
     return render(request, 'pledge/congressman_list.html', {'congressmans': congressmans})
 
-def congressman_detail(request, pk):
+def congressman_detail(request, cm_pk):
     # 국회의원 세부
-    congressman = get_object_or_404(CongressMan, pk=pk)
+    congressman = get_object_or_404(CongressMan, pk=cm_pk)
 
     context = {}
     context['congressman'] = congressman
@@ -49,9 +51,9 @@ def pledge_list(request):
 
     return render(request, 'pledge/pledge_list.html', context)
 
-def pledge_detail(request, pk):
+def pledge_detail(request, pledge_pk):
     # 공약 세부
-    pledge = get_object_or_404(Pledge, pk=pk)
+    pledge = get_object_or_404(Pledge, pk=pledge_pk)
     comment_form = CommentForm()
 
     context = {}
@@ -135,6 +137,80 @@ def pledge_comment_edit(request, pledge_pk, comment_pk):
         'form':form,
         })
 
+def pledge_like(request, pledge_pk):
+    # 공약 좋아요 버튼 클릭시
+    if request.is_ajax():
+        # ajax 요청일 경우
+        user = request.user # 요청한 유저
+        pledge = get_object_or_404(Pledge, pk=pledge_pk) # 해당 공약 인스턴스 생성
+
+        if user.like_dislike.filter(pledge_id=pledge.id).exists():
+            # 좋아요/싫어요 중 하나라도 눌렀을 경우
+            like_instance = user.like_dislike.get(pledge_id=pledge.id) # 좋아요/싫어요 인스턴스 생성
+            if like_instance.like == True:
+                # 눌려있는 버튼이 좋아요일 경우
+                LikeOrDislike.objects.filter(pledge=pledge, user=user).delete() # 인스턴스 삭제
+            else:
+                # 싫어요일 경우 좋아요로 바꾼 후 저장
+                like_instance.like = True
+                like_instance.dislike = False
+                like_instance.save()
+
+        else:
+            # 버튼이 하나도 안눌려있을 경우 새롭게 생성
+            LikeOrDislike.objects.create(
+                user=user,
+                pledge=pledge,
+                like=True,
+                dislike=False,
+                )
+
+        context = {} # 좋아요 개수와 싫어요 개수를 response에 담아 보낸다
+        context['like_count'] = pledge.get_total_like
+        context['dislike_count'] = pledge.get_total_dislike
+    else:
+        # ajax 요청이 아닐 경우
+        context = {'status': 'fail'}
+
+    # dic 형식을 json 형식으로 바꾸어 전달한다.
+    return HttpResponse(json.dumps(context), content_type='application/json')
+
+def pledge_dislike(request, pledge_pk):
+    # 공약 싫어요 버튼 클릭 시
+    if request.is_ajax():
+        # ajax 요청일 경우
+        user = request.user # 요청한 유저
+        pledge = get_object_or_404(Pledge, pk=pledge_pk) # 해당 공약 인스턴스 생성
+
+        if user.like_dislike.filter(pledge_id=pledge.id).exists():
+            # 좋아요/싫어요 중 하나라도 눌렀을 경우
+            like_instance = user.like_dislike.get(pledge_id=pledge.id) # 좋아요/싫어요 인스턴스 생성
+            if like_instance.dislike == True:
+                # 눌려있는 버튼이 싫어요일 경우
+                LikeOrDislike.objects.filter(pledge=pledge, user=user).delete() # 인스턴스 삭제
+            else:
+                # 좋아요일 경우 싫어요로 바꾼 후 저장
+                like_instance.like = False
+                like_instance.dislike = True
+                like_instance.save()
+
+        else:
+            # 버튼이 하나도 안눌려있을 경우 새롭게 생성
+            LikeOrDislike.objects.create(
+                user=user,
+                pledge=pledge,
+                like=False,
+                dislike=True,
+                )
+        context = {} # 좋아요 개수와 싫어요 개수를 response에 담아 보낸다
+        context['like_count'] = pledge.get_total_like
+        context['dislike_count'] = pledge.get_total_dislike
+    else:
+        # ajax 요청이 아닐 경우
+        context = {'status': 'fail'}
+
+    # dic 형식을 json 형식으로 바꾸어 전달한다.
+    return HttpResponse(json.dumps(context), content_type='application/json')
 
 def search(request):
     # 검색바를 통한 검색
