8264ed7f6c7ac39c17ab4a291369a9ab090c9c43
Jiyeon <hgy3047@gmail.com>
2017-05-12 14:38:51 +0900

    result_page

---
+++ commitment_verification/pledge/static/pledge/js/main.js
@@ -0,0 +1,11 @@
+ (function() {
+
+        $('.category-dropdown').on('click', '.category-change a', function(ev) {
+            if ("#" === $(this).attr('href')) {
+                ev.preventDefault();
+                var parent = $(this).parents('.category-dropdown');
+                parent.find('.change-text').html($(this).html());
+            }
+        });
+
+    }());
