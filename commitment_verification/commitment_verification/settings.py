464f9e4836f16abc4d1a1d57b381c864ccf6677c
gustos <jucie15@nate.com>
2017-05-14 16:55:42 +0900

    검색 결과창 페이지네이션 갯수수정

--- commitment_verification/commitment_verification/settings.py
+++ commitment_verification/commitment_verification/settings.py
@@ -145,3 +145,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+# El_pagination settings
+EL_PAGINATION_DEFAULT_CALLABLE_EXTREMES = 0 # 맨 첫페이지와 맨 끝페이지에서부터 몇개씩 표시할지
+EL_PAGINATION_DEFAULT_CALLABLE_AROUNDS = 3 # 현재 페이지 앞 뒤로 몇개 표시할지
+EL_PAGINATION_DEFAULT_CALLABLE_ARROWS = True # 맨 앞, 맨 뒤로 가기
